9e964b4ccb59129ef2d206258f5d1bcded52cff4
xue-think <97574730+xue-think@users.noreply.github.com>
2024-03-21 19:56:07 -0400

    Update tag_generator.rb

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -1,27 +1,26 @@
 module Jekyll
-class TagPageGenerator < Generator
-  def generate(site)
-    if site.layouts.key? 'tag'
-      site.tags.each_key do |tag|
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.keys.each do |tag|
         site.pages << TagPage.new(site, site.source, tag)
       end
     end
   end
-end
 
-class TagPage < Page
-  def initialize(site, base, tag)
-    @site = site
-    @base = base
-    @dir = File.join('tags', tag.downcase.gsub(' ', '-'))
-    @name = 'index.html'
+  class TagPage < Page
+    def initialize(site, base, tag)
+      @site = site
+      @base = base
+      @dir = File.join('tags', tag.downcase.gsub(' ', '-'))
+      @name = 'index.html'
 
-    self.process(@name)
-    self.data = {
-      'layout' => 'tag',
-      'tag' => tag,
-      'permalink' => "/tags/#{tag.downcase.gsub(' ', '-')}/"
-    }
+      self.process(@name)
+      self.data = {
+        'layout' => 'tag',
+        'tag' => tag,
+      }
+    end
   end
-end
 end
