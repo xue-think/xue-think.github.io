d56547b528b014d7e1061cc04fd4c6f5e12f3bee
xue-think <97574730+xue-think@users.noreply.github.com>
2026-02-03 21:29:09 -0500

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,7 +1,17 @@
 import type { UserConfig } from '~/types'
 
-export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+export const userConfig: Partial<ThemeConfig> = {
+  site: {
+    title: '表里之间',
+    subtitle: 'In Between',
+    author: 'o-o',
+    description: 'A brief moment in between',
+    website: 'https://dr2quan.github.io',
+    categoryMap: [
+      { name: '表里之间', path: 'in-between' },
+    ],
+  },
+  appearance: {
+    theme: 'light',
+  },
 }
